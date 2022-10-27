b8eb162e6cee4253801fd5ba7e026c94f73cda16
Shivam Panda <panda.shivam81@bloomfield.org>
2022-10-27 09:16:51 -0400

    Data Gathering

--- contour.py
+++ contour.py
@@ -1,34 +1,45 @@
 import cv2
 import numpy as np
 
+'''
+Data Values
+36" -> 681.218660968661
+24" -> 1403.6509615384616
+'''
+
 cap = cv2.VideoCapture(0)
 
 def nothing(x):
     pass
 
-cv2.namedWindow('result')
-cv2.createTrackbar('h_low', 'result', 0, 179, nothing)
-cv2.createTrackbar('s_low', 'result', 0, 255, nothing)
-cv2.createTrackbar('v_low', 'result', 0, 255, nothing)
+# cv2.namedWindow('result')
+# cv2.createTrackbar('h_low', 'result', 0, 179, nothing)
+# cv2.createTrackbar('s_low', 'result', 0, 255, nothing)
+# cv2.createTrackbar('v_low', 'result', 0, 255, nothing)
 
-cv2.createTrackbar('h_upper', 'result', 0, 179, nothing)
-cv2.createTrackbar('s_upper', 'result', 0, 255, nothing)
-cv2.createTrackbar('v_upper', 'result', 0, 255, nothing)
+# cv2.createTrackbar('h_upper', 'result', 0, 179, nothing)
+# cv2.createTrackbar('s_upper', 'result', 0, 255, nothing)
+# cv2.createTrackbar('v_upper', 'result', 0, 255, nothing)
+
+areas = []
 
 while True:
     _, frame = cap.read()
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-    h_low=  cv2.getTrackbarPos('h_low', 'result')
-    s_low=  cv2.getTrackbarPos('s_low', 'result')
-    v_low=  cv2.getTrackbarPos('v_low', 'result')
+    # h_low=  cv2.getTrackbarPos('h_low', 'result')
+    # s_low=  cv2.getTrackbarPos('s_low', 'result')
+    # v_low=  cv2.getTrackbarPos('v_low', 'result')
 
-    h_upper =  cv2.getTrackbarPos('h_upper', 'result')
-    s_upper =  cv2.getTrackbarPos('s_upper', 'result')
-    v_upper =  cv2.getTrackbarPos('v_upper', 'result')
+    # h_upper =  cv2.getTrackbarPos('h_upper', 'result')
+    # s_upper =  cv2.getTrackbarPos('s_upper', 'result')
+    # v_upper =  cv2.getTrackbarPos('v_upper', 'result')
 
-    low_orange = np.array([h_low,s_low,v_low])
-    high_orange = np.array([h_upper,s_upper,v_upper])
+    low_orange = np.array([18,82,186])
+    high_orange = np.array([22,144,255])
+
+    # low_orange = np.array([h_low, s_low, v_low])
+    # high_orange = np.array([h_upper, s_upper, v_upper])
 
     mask = cv2.inRange(hsv, low_orange, high_orange)
     kernel = np.ones((5,5), np.uint8)
@@ -38,8 +49,10 @@ while True:
 
     for cnt in contours:
         area = cv2.contourArea(cnt)
-        if area > 400:
-            cv2.drawContours(frame, [cnt], 0, (0, 0, 0), 5)
+        approx = cv2.approxPolyDP(cnt, 0.05 * cv2.arcLength(cnt, True), True)
+        if area > 400 and len(approx) == 4:
+            areas.append(area)
+            cv2.drawContours(frame, [approx], 0, (0, 0, 0), 5)
 
     cv2.imshow("Frame", frame)
     cv2.imshow("Mask", mask)
@@ -48,5 +61,9 @@ while True:
     if key == 27:
         break
     
-cap.release()    
+cap.release()
+sum = 0
+for i in areas:
+    sum += i
+print(sum/len(areas))
 cv2.destroyAllWindows()
